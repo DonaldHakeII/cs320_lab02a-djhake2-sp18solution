6469e6f4334ed616cf646ab4ec77c5af25b1214c
DonaldHakeII <djhake2@ycp.edu>
2022-02-11 11:42:36 -0500

    generatring a merge conflict

--- CS320_djhake2/src/edu/ycp/cs320/djhake2/servlet/AddNumbersServlet.java
+++ CS320_djhake2/src/edu/ycp/cs320/djhake2/servlet/AddNumbersServlet.java
@@ -45,6 +45,7 @@ public class AddNumbersServlet extends HttpServlet {
 			
 			// check for errors in the form data before using is in a calculation
 			// null means that a number was not entered
+			// a different change in the comments to generate a merge conflict
 			if (first == null || second == null || third == null) {
 				errorMessage = "Please specify three numbers";
 			}
